52172473d4696c1be505e56b256b94ab1145697d
Darrel Pol <poldarre.dev@gmail.com>
2022-01-10 22:52:04 -0600

    adding neccesarry files for creating auth token

--- src/ronin/ronin.service.ts
+++ src/ronin/ronin.service.ts
@@ -1,4 +1,7 @@
 import { MMR } from "./ronin.interfaces";
+import Web3 from "web3";
+import { mainnet, reqConfig } from "../shared/shared.service";
+const web3 = new Web3();
 
 /**
  * Data Model Interfaces
@@ -48,3 +51,56 @@ export const getMMRbyRoninAddress = async (roninAddress: string) => {
         return result;
     }
 }
+
+export const fetchData = async (
+    postData: { [key: string]: any }
+): Promise<any> => {
+    const url = 'https://graphql-gateway.axieinfinity.com/graphql';
+    const { data, status } = await axios.post(url, postData, reqConfig);
+
+    if (status < 200 && status >= 300) {
+        throw Error('Axie Infinity API have a problem');
+    }
+
+    return data;
+};
+
+export const getRandomMessage = async () => {
+    try {
+        const response = await fetchData({
+            'operationName': "CreateRandomMessage",
+            'query': "mutation CreateRandomMessage {\n  createRandomMessage\n}\n",
+            'variables': {}
+        });
+        return {
+            status: true,
+            message: response.data.createRandomMessage
+        }
+    } catch (err) {
+        console.log(err);
+        return {
+            message: '',
+            status: false
+        }
+    }
+}
+
+export const submitSignature = async (
+    accountAddress: string,
+    privateKey: string,
+    randMessage: string) => {
+    try {
+        let hexSignature = web3.eth.accounts.sign(randMessage, privateKey);
+        const signature = hexSignature['signature'];
+
+        const response = await fetchData({
+            "operationName": "CreateAccessTokenWithSignature",
+            "variables": { "input": { "mainnet": mainnet, "owner": accountAddress, "message": randMessage, "signature": signature } },
+            "query": "mutation CreateAccessTokenWithSignature($input: SignatureInput!) {\n  createAccessTokenWithSignature(input: $input) {\n    newAccount\n    result\n    accessToken\n    __typename\n  }\n}\n"
+        });
+        return response.data.createAccessTokenWithSignature.accessToken;
+    } catch (err) {
+        console.log(err);
+        return false;
+    }
+};
--- src/shared/shared.service.ts
+++ src/shared/shared.service.ts
@@ -12,6 +12,8 @@
 /**
  * Service Methods
  */
+export const mainnet = 'ronin';
+
 export const isProduction = () => {
     return process.env.environment == "prod";
 }
@@ -19,3 +21,13 @@ export const isProduction = () => {
 export const getHost = () => {
     return isProduction() ? `${process.env.protocol}://${process.env.host}` : `${process.env.protocol}://${process.env.host}:${process.env.PORT}`;
 }
+
+export const reqConfig = {
+    headers: {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
+        "content-type": "application/json",
+        "accept-language": "en-US,en;q=0.9",
+        "origin": "https://marketplace.axieinfinity.com",
+        "authority": "graphql-gateway.axieinfinity.com"
+    }
+};
