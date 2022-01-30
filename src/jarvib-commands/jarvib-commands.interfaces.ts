daae34da545fc9dfbb31403f51c6d09588d5a564
Darrel Pol <81864872+poldarreldev@users.noreply.github.com>
2022-01-30 17:33:10 -0600

    getqrcode (#46)
    * getqrcode
    
    * removing unwanted file
    
    * adding jadewick logo
    
    * changing log title
    
    Co-authored-by: Darrel Pol <poldarre.dev@gmail.com>

--- src/jarvib-commands/jarvib-commands.interfaces.ts
+++ src/jarvib-commands/jarvib-commands.interfaces.ts
@@ -2,7 +2,8 @@ export enum Commands {
   PING = "PING",
   GETSCHOLARS = "GETSCHOLARS",
   GETMMR = "GETMMR",
-  GETSLP = "GETSLP"
+  GETSLP = "GETSLP",
+  GENERATEMYQR = "GENERATEMYQR"
 }
 
 export const help: string = `Hey, you can tell me what to do using these commands:
@@ -10,4 +11,5 @@ ${"```"}\n
 !jarvib ${Commands.PING} - to get pong.\n
 !jarvib ${Commands.GETSCHOLARS} - to get the link to view scholars and ronin addresses.\n
 !jarvib ${Commands.GETMMR} {roninAddress} - to get the link to view scholars and ronin addresses. (e. g. ${"`"}!jarvib getmmr ronin:94f1...9dc${"`"})\n
+!jarvib ${Commands.GENERATEMYQR} - to generate a new QR code. QR Code will be sent in private to the requestor if available.\n
 ${"```"}\n`
