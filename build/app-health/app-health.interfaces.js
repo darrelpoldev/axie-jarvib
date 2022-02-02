0fed5b6dd649414248825169b7b530a87a4873c9
Darrel Pol <81864872+poldarreldev@users.noreply.github.com>
2022-02-01 21:33:41 -0600

    Staging (#64)
    * fixing tsc bug (#53)
    
    Co-authored-by: Darrel Pol <poldarre.dev@gmail.com>
    
    * Bugfix3 (#55)
    
    * fixing tsc bug
    
    * tsc script
    
    Co-authored-by: Darrel Pol <poldarre.dev@gmail.com>
    
    * Bugfix3 (#57)
    
    * fixing tsc bug
    
    * tsc script
    
    * tsc changes
    
    Co-authored-by: Darrel Pol <poldarre.dev@gmail.com>
    
    * Bugfix3 (#59)
    
    * fixing tsc bug
    
    * tsc script
    
    * tsc changes
    
    * finally fixing the build problem
    
    Co-authored-by: Darrel Pol <poldarre.dev@gmail.com>
    
    * Bugfix3 (#61)
    
    * fixing tsc bug
    
    * tsc script
    
    * tsc changes
    
    * finally fixing the build problem
    
    Co-authored-by: Darrel Pol <poldarre.dev@gmail.com>
    
    * Bugfix3 (#63)
    
    * fixing tsc bug
    
    * tsc script
    
    * tsc changes
    
    * finally fixing the build problem
    
    * testing packagejson
    
    Co-authored-by: Darrel Pol <poldarre.dev@gmail.com>
    
    Co-authored-by: Darrel Pol <poldarre.dev@gmail.com>

---
+++ build/app-health/app-health.interfaces.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AppHealthStatus = void 0;
+var AppHealthStatus;
+(function (AppHealthStatus) {
+    AppHealthStatus["GREEN"] = "HEALTHY";
+    AppHealthStatus["RED"] = "UNHEALTHY";
+})(AppHealthStatus = exports.AppHealthStatus || (exports.AppHealthStatus = {}));
